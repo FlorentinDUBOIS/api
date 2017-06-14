05afa5163740d289ef34407f95d1a4d275e628fa
contact@florentin-dubois.fr <contact@florentin-dubois.fr>
2017-06-14 15:49:21 +0200

    fix(depencies): Update github.com/FlorentinDUBOIS/gin-jwt-forwarder

--- controllers/auth.go
+++ controllers/auth.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/FlorentinDUBOIS/bouncer/services"
+	"github.com/FlorentinDUBOIS/gin-jwt-forwarder/handlers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,5 +14,7 @@ func (authController *AuthController) Register(pGroup *gin.RouterGroup) {
 	pGroup.Use()
 	{
 		pGroup.POST("/", services.JWTMiddleware.LoginHandler)
+
+		pGroup.GET("/validate", handlers.Ok)
 	}
 }
