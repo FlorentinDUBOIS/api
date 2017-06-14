7644fc90bd138bedd2d42e7e01dde1c3b59f43f3
contact@florentin-dubois.fr <contact@florentin-dubois.fr>
2017-06-14 11:01:54 +0200

    feat(user): Append some test on user input

--- services/user.go
+++ services/user.go
@@ -1,6 +1,11 @@
 package services
 
-import "github.com/FlorentinDUBOIS/bouncer/provider/api"
+import (
+	"errors"
+
+	"github.com/FlorentinDUBOIS/bouncer/provider/api"
+	"github.com/badoux/checkmail"
+)
 
 // UserService structure
 type UserService struct{}
@@ -17,7 +22,15 @@ func (*UserService) FindOne(pUUID string) *api.User {
 
 // Save an user
 func (*UserService) Save(pUser *api.User) (*api.User, error) {
+	if pUser == nil || pUser.FirstName == nil || pUser.LastName == nil || pUser.Email == nil || pUser.Password == nil {
+		return nil, errors.New("You must set a FirstName, LastName, Email and Password to your user in order to be created")
+	}
+
 	if err := pUser.EncryptPassword(); err != nil {
+		return nil, err
+	}
+
+	if err := checkmail.ValidateFormat(*pUser.Email); err != nil {
 		return nil, err
 	}
 
@@ -31,8 +44,18 @@ func (*UserService) Save(pUser *api.User) (*api.User, error) {
 
 // Update an user
 func (*UserService) Update(pUUID string, pUser *api.User) (*api.User, error) {
+	if pUser == nil {
+		return nil, errors.New("You must give at least one user field to update")
+	}
+
 	if pUser.Password != nil {
 		if err := pUser.EncryptPassword(); err != nil {
+			return nil, err
+		}
+	}
+
+	if pUser.Email != nil {
+		if err := checkmail.ValidateFormat(*pUser.Email); err != nil {
 			return nil, err
 		}
 	}
